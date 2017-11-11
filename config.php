64f83e2b4f656e329d36c683647215bfda0f7f7b
liuzhongsheng <996674366@qq.com>
2017-11-11 06:33:09 -0600

    新增项目文件

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'start_using'   =>  'off',  //on 开 off关闭
+    'doc_dir'       => 'Word',  //文档保存目录默认
+    'api_v1.0_list'=> [
+        'apps\v10\controller\search',
+        'apps\v10\controller\user'
+    ],
+    'v1.0'          => [
+        ['2017-11-10 09:47', '注册用户接口', '新增site_id', '刘中胜']
+    ],
+];
